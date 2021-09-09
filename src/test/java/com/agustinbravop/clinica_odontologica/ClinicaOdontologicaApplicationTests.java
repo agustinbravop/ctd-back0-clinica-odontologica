c16b67ad4e5d2a474d918772ec35d08def11880c
AgusBrr <anbravoperez@gmail.com>
2021-09-09 17:49:42 -0300

    initialize project structure

---
+++ src/test/java/com/agustinbravop/clinica_odontologica/ClinicaOdontologicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustinbravop.clinica_odontologica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaOdontologicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
