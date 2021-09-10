d74f91472c722d8ab1d8653cc50f35f455cb1318
AgusBrr <anbravoperez@gmail.com>
2021-09-10 16:38:58 -0300

    logger (log4j) configurado

---
+++ src/main/java/com/agustinbravop/clinica_odontologica/dao/config/ConfiguracionJDBC.java
@@ -0,0 +1,40 @@
+package com.agustinbravop.clinica_odontologica.dao.config;
+
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConfiguracionJDBC {
+    private String jdbcDriver;
+    private String dbUrl;
+    private String user;
+    private String password;
+
+    public ConfiguracionJDBC() {
+        this.jdbcDriver = "org.h2.Driver";
+        this.dbUrl = "jdbc:h2:~/ctd_back1_clinica_odontologica;INIT=RUNSCRIPT FROM 'create.sql'";
+        this.user = "sa";
+        this.password = null;
+    }
+
+    public ConfiguracionJDBC(String jdbcDriver, String dbUrl, String user, String password) {
+        this.jdbcDriver = jdbcDriver;
+        this.dbUrl = dbUrl;
+        this.user = user;
+        this.password = password;
+    }
+
+    public Connection conectarConDB() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(dbUrl, user, password);
+        } catch (SQLException e) {
+            Logger logger = Logger.getLogger(ConfiguracionJDBC.class);
+            logger.debug(e);
+        }
+
+        return connection;
+    }
+}
