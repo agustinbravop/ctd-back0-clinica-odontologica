43830ca7e556d14c3f6763a0fe45c12dcab8b4c8
AgusBrr <anbravoperez@gmail.com>
2021-09-19 20:05:35 -0300

    Agregada dependencia ModdelMapper

---
+++ src/main/java/com/agustinbravop/clinica_odontologica/config/ApplicationConfig.java
@@ -0,0 +1,19 @@
+package com.agustinbravop.clinica_odontologica.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.NamingConventions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public ModelMapper getModelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setSourceNamingConvention(NamingConventions.JAVABEANS_ACCESSOR);
+        return modelMapper;
+    }
+}
